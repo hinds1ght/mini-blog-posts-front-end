91f9a50261632622805d15edfcf7b9049e4ed5d2
hinds1ght <markgil_yu@yahoo.com>
2025-07-03 16:31:42 +0800

    Header comments cleanup

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom"; 
 import "./Header.css";
-//import logo from "../assets/logo.png"; 
+ 
 
 const Header = () => {
   return (
     <header className="header">
       <div className="header-left">
-        {/*<img src={logo} alt="Logo" className="logo" />*/}
         <h1 className="title">Mini Blog Post App</h1>
       </div>
       <nav className="nav">
