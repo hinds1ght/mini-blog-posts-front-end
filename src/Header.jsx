0188bbf78ca682443face9e52d2aab48620a5753
hinds1ght <markgil_yu@yahoo.com>
2025-07-03 14:49:42 +0800

    add header and links placeholder

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,5 +1,22 @@
-function Header() {
-    return <h1>Mini Blog Post</h1>
-}
+import React from "react";
+import { Link } from "react-router-dom"; 
+import "./Header.css";
+//import logo from "../assets/logo.png"; 
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-left">
+        {/*<img src={logo} alt="Logo" className="logo" />*/}
+        <h1 className="title">Mini Blog Post App</h1>
+      </div>
+      <nav className="nav">
+        <Link to="/" className="nav-link">Home</Link>
+        <Link to="/about" className="nav-link">About</Link>
+        <Link to="/contact" className="nav-link">Contact</Link>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
